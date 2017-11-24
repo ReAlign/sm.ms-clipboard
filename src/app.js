67186f6b380a85791f1a18d376b42d187e4ef35f
ReAlign <realign@yeah.net>
2017-11-24 19:08:11 +0800

    re:update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,48 @@
 $(document).ready(function () {
+    var SMMS = {
+        initStyles: function() {
+            $('#urlcode').css({
+                display: 'block',
+                height: '0',
+                overflow: 'hidden'
+            });
+        },
+        getUrlList: function(cbEvt) {
+            var args = arguments;
+            var context = this;
+
+            setTimeout(function() {
+                var str = $('#urlcode').html();
+
+                var arr = str.split(/\s+/);
+                arr = arr.filter(function(item) {
+                    return item != '';
+                });
+
+                console.log(arr);
+
+                cbEvt(arr);
+            }, 1000);
+        },
+        renderClipboard: function(arr) {
+            // debugger
+            console.log(arr);
+
+            // var s_str = '<div style="' + wrapStyle + '"></div>';
+            // var _str = '';
+
+            // $('#urlcodes pre').append();
+        }
+    };
+
+    SMMS.initStyles();
+
     $(document).ajaxStart(function () {
         console.log('start');
     });
+
     $(document).ajaxComplete(function () {
         console.log('complete');
-        $('#urlcodes pre').append('<button>12</button>');
-        setTimeout(function() {
-            var str = $('#urlcode').html();
-
-            var arr = str.split(/\s+/);
-            arr.forEach(function(item, index) {
-                if(item == '') {
-                    arr.splice(index, 1);
-                }
-            });
-            console.log(arr);
-        }, 1000);
+        SMMS.getUrlList(SMMS.renderClipboard);
     });
 });
